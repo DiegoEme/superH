c4a48c8db646f289ecc57d38b21f55cdd0a8248f
unknown <soterg.uhdeia@gmail.com>
2021-09-11 12:55:55 -0500

    buscar superheroe

--- src/app/heroes/pages/buscar/buscar.component.ts
+++ src/app/heroes/pages/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe } from '../../interfaces/heroe.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-buscar',
@@ -8,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuscarComponent implements OnInit {
 
-  constructor() { }
+
+  termino: string =""
+  heroes: Heroe[] = []
+  heroeSeleccionado: Heroe | undefined
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit(): void {
+  }
+
+  buscando(){
+    this.heroesService.getSugerencias(this.termino.trim()).subscribe(heroes => this.heroes = heroes)
+  }
+
+  opcionSeleccionada(event: any){
+    if(!event.option.value){
+      this.heroeSeleccionado = undefined;
+      console.log("nohay mada pa")
+      return;
+    }
+    const heroe: Heroe = event.option.value
+    this.termino = heroe.superhero
+
+    this.heroesService.getHeroePorId(heroe.id).subscribe(heroe => this.heroeSeleccionado = heroe)
   }
 
 }
